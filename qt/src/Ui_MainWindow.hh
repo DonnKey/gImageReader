7f0485c33276087692baa4d5daca5a5cc9c663dc
donnKey <aesopPlayer@gmail.com>
2022-02-19 12:32:10 -0800

    Build speedup: get code out of Ui headers. (Added copyrights.)

--- qt/src/Ui_MainWindow.hh
+++ qt/src/Ui_MainWindow.hh
@@ -1,3 +1,22 @@
+/* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * Ui_MainWindow.hh
+ * Copyright (C) 2013-2020 Sandro Mani <manisandro@gmail.com>
+ *
+ * gImageReader is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gImageReader is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef UI_MAINWINDOW_HH
 #define UI_MAINWINDOW_HH
 
@@ -40,202 +59,7 @@ public:
 	QWidgetAction* actionRotate;
 	QWidgetAction* actionPage;
 
-
-	void setupUi(QMainWindow* MainWindow) {
-		Ui_MainWindow::setupUi(MainWindow);
-
-		// Do remaining things which are not possible in designer
-		toolBarMain->setContextMenuPolicy(Qt::PreventContextMenu);
-
-		// Remove & from some labels which designer insists in adding
-		dockWidgetSources->setWindowTitle(gettext("Sources"));
-		dockWidgetOutput->setWindowTitle(gettext("Output"));
-
-		// Hide image controls widget
-		widgetImageControls->setVisible(false);
-
-		// Rotate spinbox
-		frameRotation = new QFrame(MainWindow);
-		frameRotation->setFrameShape(QFrame::StyledPanel);
-		frameRotation->setFrameShadow(QFrame::Sunken);
-		frameRotation->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-		frameRotation->setToolTip(gettext("Rotate page"));
-
-		QHBoxLayout* layoutRotation = new QHBoxLayout(frameRotation);
-		layoutRotation->setContentsMargins(1, 1, 1, 1);
-		layoutRotation->setSpacing(0);
-
-		actionRotateCurrentPage = new QAction(QIcon(":/icons/rotate_page"), gettext("Rotate current page"), MainWindow);
-		actionRotateAllPages = new QAction(QIcon(":/icons/rotate_pages"), gettext("Rotate all pages"), MainWindow);
-		actionRotateAuto = new QAction(QIcon(":/icons/rotate_auto"), gettext("Auto rotate when recognizing"), MainWindow);
-
-		menuRotation = new QMenu(MainWindow);
-		menuRotation->addAction(actionRotateCurrentPage);
-		menuRotation->addAction(actionRotateAllPages);
-		menuRotation->addAction(actionRotateAuto);
-
-		toolButtonRotation = new QToolButton(MainWindow);
-		toolButtonRotation->setIcon(QIcon(":/icons/rotate_pages"));
-		toolButtonRotation->setToolTip(gettext("Select rotation mode"));
-		toolButtonRotation->setPopupMode(QToolButton::InstantPopup);
-		toolButtonRotation->setAutoRaise(true);
-		toolButtonRotation->setMenu(menuRotation);
-
-		layoutRotation->addWidget(toolButtonRotation);
-
-		spinBoxRotation = new QDoubleSpinBox(MainWindow);
-		spinBoxRotation->setRange(0.0, 359.9);
-		spinBoxRotation->setDecimals(1);
-		spinBoxRotation->setSingleStep(0.1);
-		spinBoxRotation->setWrapping(true);
-		spinBoxRotation->setFrame(false);
-		spinBoxRotation->setKeyboardTracking(false);
-		spinBoxRotation->setSizePolicy(spinBoxRotation->sizePolicy().horizontalPolicy(), QSizePolicy::MinimumExpanding);
-		layoutRotation->addWidget(spinBoxRotation);
-
-		actionRotate = new QWidgetAction(MainWindow);
-		actionRotate->setDefaultWidget(frameRotation);
-
-		toolBarMain->insertAction(actionImageControls, actionRotate);
-
-		// Page spinbox
-		framePage = new QFrame(MainWindow);
-		framePage->setFrameShape(QFrame::StyledPanel);
-		framePage->setFrameShadow(QFrame::Sunken);
-		framePage->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-		framePage->setToolTip(gettext("Select page"));
-
-		QHBoxLayout* layoutPage = new QHBoxLayout(framePage);
-		layoutPage->setContentsMargins(1, 1, 1, 1);
-		layoutPage->setSpacing(0);
-
-		QToolButton* toolButtonPage = new QToolButton(MainWindow);
-		toolButtonPage->setIcon(QPixmap(":/icons/page"));
-		toolButtonPage->setEnabled(false);
-		toolButtonPage->setAutoRaise(true);
-		layoutPage->addWidget(toolButtonPage);
-
-		spinBoxPage = new QSpinBox(MainWindow);
-		spinBoxPage->setRange(1, 1);
-		spinBoxPage->setFrame(false);
-		spinBoxPage->setKeyboardTracking(false);
-		spinBoxPage->setSizePolicy(spinBoxPage->sizePolicy().horizontalPolicy(), QSizePolicy::MinimumExpanding);
-		layoutPage->addWidget(spinBoxPage);
-
-		actionPage = new QWidgetAction(MainWindow);
-		actionPage->setDefaultWidget(framePage);
-
-		toolBarMain->insertAction(actionImageControls, actionPage);
-		actionPage->setVisible(false);
-
-		QFont smallFont;
-		smallFont.setPointSizeF(smallFont.pointSizeF() * 0.9);
-
-		// OCR mode button
-		QWidget* ocrModeWidget = new QWidget();
-		ocrModeWidget->setLayout(new QVBoxLayout());
-		ocrModeWidget->layout()->setContentsMargins(0, 0, 0, 0);
-		ocrModeWidget->layout()->setSpacing(0);
-		QLabel* outputModeLabel = new QLabel(gettext("OCR mode:"));
-		outputModeLabel->setFont(smallFont);
-		ocrModeWidget->layout()->addWidget(outputModeLabel);
-		comboBoxOCRMode = new QComboBox();
-		comboBoxOCRMode->setFont(smallFont);
-		comboBoxOCRMode->setFrame(false);
-		comboBoxOCRMode->setCurrentIndex(-1);
-		ocrModeWidget->layout()->addWidget(comboBoxOCRMode);
-		toolBarMain->insertWidget(actionAutodetectLayout, ocrModeWidget);
-
-		actionAutodetectLayout->setVisible(false);
-
-		// Recognize and language button
-		toolButtonRecognize = new QToolButton(MainWindow);
-		toolButtonRecognize->setIcon(QIcon::fromTheme("insert-text"));
-		toolButtonRecognize->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-		toolButtonRecognize->setFont(smallFont);
-		toolBarMain->insertWidget(actionToggleOutputPane, toolButtonRecognize);
-
-		toolButtonLanguages = new QToolButton(MainWindow);
-		toolButtonLanguages->setIcon(QIcon::fromTheme("applications-education-language"));
-		toolButtonLanguages->setPopupMode(QToolButton::InstantPopup);
-		toolBarMain->insertWidget(actionToggleOutputPane, toolButtonLanguages);
-
-		toolBarMain->insertSeparator(actionToggleOutputPane);
-
-		// Spacer before app menu button
-		QWidget* toolBarMainSpacer = new QWidget(toolBarMain);
-		toolBarMainSpacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-		toolBarMain->addWidget(toolBarMainSpacer);
-
-		// KeyMap window
-		toolButtonEditKeyMap = new QToolButton(MainWindow);
-		toolButtonEditKeyMap->setAutoRaise(true);
-		toolButtonEditKeyMap->setIcon(QIcon::fromTheme("preferences-desktop-keyboard-symbolic.symbolic"));
-		toolButtonEditKeyMap->setPopupMode(QToolButton::InstantPopup);
-		toolButtonEditKeyMap->setToolTip("Map keys to actions");
-		toolBarMain->addWidget(toolButtonEditKeyMap);
-
-		// App menu
-		menuAppMenu = new QMenu(MainWindow);
-
-		actionRedetectLanguages = new QAction(QIcon::fromTheme("view-refresh"), gettext("Redetect Languages"), MainWindow);
-		menuAppMenu->addAction(actionRedetectLanguages);
-
-		actionManageLanguages = new QAction(QIcon::fromTheme("applications-education-language"), gettext("Manage Languages"), MainWindow);
-		menuAppMenu->addAction(actionManageLanguages);
-
-		actionPreferences = new QAction(QIcon::fromTheme("preferences-system"), gettext("Preferences"), MainWindow);
-		menuAppMenu->addAction(actionPreferences);
-
-		menuAppMenu->addSeparator();
-
-		actionHelp = new QAction(QIcon::fromTheme("help-contents"), gettext("Help"), MainWindow);
-		menuAppMenu->addAction(actionHelp);
-
-		actionAbout = new QAction(QIcon::fromTheme("help-about"), gettext("About"), MainWindow);
-		menuAppMenu->addAction(actionAbout);
-
-		// App menu button
-		toolButtonAppMenu = new QToolButton(MainWindow);
-		toolButtonAppMenu->setIcon(QIcon::fromTheme("preferences-system"));
-		toolButtonAppMenu->setPopupMode(QToolButton::InstantPopup);
-		toolButtonAppMenu->setMenu(menuAppMenu);
-		toolBarMain->addWidget(toolButtonAppMenu);
-
-		// Sources toolbar
-		actionSourceFolder = new QAction(QIcon::fromTheme("folder-open"), gettext("Add folder"), MainWindow);
-		actionSourcePaste = new QAction(QIcon::fromTheme("edit-paste"), gettext("Paste"), MainWindow);
-		actionSourceScreenshot = new QAction(QIcon::fromTheme("camera-photo"), gettext("Take Screenshot"), MainWindow);
-
-		toolButtonSourceAdd = new QToolButton(MainWindow);
-		toolButtonSourceAdd->setIcon(QIcon::fromTheme("document-open"));
-		toolButtonSourceAdd->setText(gettext("Add Images"));
-		toolButtonSourceAdd->setToolTip(gettext("Add images"));
-		toolButtonSourceAdd->setPopupMode(QToolButton::MenuButtonPopup);
-
-		actionSourceRemove = new QAction(QIcon::fromTheme("list-remove"), gettext("Remove Image"), MainWindow);
-		actionSourceRemove->setToolTip(gettext("Remove image from list"));
-		actionSourceRemove->setEnabled(false);
-		actionSourceDelete = new QAction(QIcon::fromTheme("user-trash"), gettext("Delete Image"), MainWindow);
-		actionSourceDelete->setToolTip(gettext("Delete image"));
-		actionSourceDelete->setEnabled(false);
-		actionSourceClear = new QAction(QIcon::fromTheme("edit-clear"), gettext("Clear List"), MainWindow);
-		actionSourceClear->setToolTip(gettext("Clear list"));
-		actionSourceClear->setEnabled(false);
-
-		toolBarSources = new QToolBar(MainWindow);
-		toolBarSources->setToolButtonStyle(Qt::ToolButtonIconOnly);
-		toolBarSources->setIconSize(QSize(1, 1) * toolBarSources->style()->pixelMetric(QStyle::PM_SmallIconSize));
-		toolBarSources->addWidget(toolButtonSourceAdd);
-		toolBarSources->addAction(actionSourceFolder);
-		toolBarSources->addAction(actionSourcePaste);
-		toolBarSources->addAction(actionSourceScreenshot);
-		toolBarSources->addSeparator();
-		toolBarSources->addAction(actionSourceRemove);
-		toolBarSources->addAction(actionSourceDelete);
-		toolBarSources->addAction(actionSourceClear);
-		static_cast<QVBoxLayout*>(tabSources->layout())->insertWidget(0, toolBarSources);
-	}
+	void setupUi(QMainWindow* MainWindow);
 };
 
 #endif // UI_MAINWINDOW_HH
--- qt/src/hocr/Ui_OutputEditorHOCR.hh
+++ qt/src/hocr/Ui_OutputEditorHOCR.hh
@@ -1,3 +1,22 @@
+/* -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * Ui_OutputEditorHOCR.hh
+ * Copyright (C) 2013-2020 Sandro Mani <manisandro@gmail.com>
+ *
+ * gImageReader is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gImageReader is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef UI_OUTPUTEDITORHOCR_HH
 #define UI_OUTPUTEDITORHOCR_HH
 
@@ -61,149 +80,7 @@ public:
 	QDialog* outputDialog;
 	Ui::OutputSettingsDialog outputDialogUi;
 
-	void setupUi(QWidget* widget) {
-		widget->setLayout(new QVBoxLayout());
-		widget->layout()->setContentsMargins(0, 0, 0, 0);
-		widget->layout()->setSpacing(0);
-
-		// Output toolbar
-		actionInsertModeAppend = new QAction(QIcon(":/icons/ins_hocr_append"), gettext("Append new output after last page"), widget);
-		actionInsertModeBefore = new QAction(QIcon(":/icons/ins_hocr_before"), gettext("Insert new output before current page"), widget);
-
-		menuInsertMode = new QMenu(widget);
-		menuInsertMode->addAction(actionInsertModeAppend);
-		menuInsertMode->addAction(actionInsertModeBefore);
-
-
-		toolButtonInsertMode = new QToolButton(widget);
-		toolButtonInsertMode->setIcon(QIcon(":/icons/ins_hocr_append"));
-		toolButtonInsertMode->setToolTip(gettext("Select insert mode"));
-		toolButtonInsertMode->setPopupMode(QToolButton::InstantPopup);
-		toolButtonInsertMode->setMenu(menuInsertMode);
-
-		actionOpenAppend = new QAction(gettext("Append document after last page"), widget);
-		actionOpenInsertBefore = new QAction(gettext("Insert document before current page"), widget);
-
-		menuOpen = new QMenu(widget);
-		menuOpen->addAction(actionOpenAppend);
-		menuOpen->addAction(actionOpenInsertBefore);
-
-		toolButtonOpen = new QToolButton(widget);
-		toolButtonOpen->setIcon(QIcon::fromTheme("document-open"));
-		toolButtonOpen->setText(gettext("Open hOCR file"));
-		toolButtonOpen->setToolTip(gettext("Open hOCR file"));
-		toolButtonOpen->setPopupMode(QToolButton::MenuButtonPopup);
-		toolButtonOpen->setMenu(menuOpen);
-
-		actionOutputSaveHOCR = new QAction(QIcon::fromTheme("document-save-as"), gettext("Save as hOCR text"), widget);
-		actionOutputSaveHOCR->setToolTip(gettext("Save as hOCR text"));
-		actionOutputSaveHOCR->setEnabled(false);
-		QMenu* exportMenu = new QMenu();
-		actionOutputExportText = new QAction(QIcon::fromTheme("text-plain"), gettext("Export to plain text"), widget);
-		actionOutputExportText->setToolTip(gettext("Export to plain text"));
-		exportMenu->addAction(actionOutputExportText);
-		actionOutputExportIndentedText = new QAction(QIcon::fromTheme("text-plain"), gettext("Export to plain text, preserve whitespace"), widget);
-		actionOutputExportIndentedText->setToolTip(gettext("Export to plain text, preserve whitespace"));
-		exportMenu->addAction(actionOutputExportIndentedText);
-		actionOutputExportPDF = new QAction(QIcon::fromTheme("application-pdf"), gettext("Export to PDF"), widget);
-		actionOutputExportPDF->setToolTip(gettext("Export to PDF"));
-		exportMenu->addAction(actionOutputExportPDF);
-		actionOutputExportODT = new QAction(QIcon::fromTheme("x-office-document"), gettext("Export to ODT"), widget);
-		actionOutputExportODT->setToolTip(gettext("Export to ODT"));
-		exportMenu->addAction(actionOutputExportODT);
-		toolButtonOutputExport = new QToolButton(widget);
-		toolButtonOutputExport->setIcon(QIcon::fromTheme("document-export"));
-		toolButtonOutputExport->setText(gettext("Export"));
-		toolButtonOutputExport->setToolTip(gettext("Export"));
-		toolButtonOutputExport->setEnabled(false);
-		toolButtonOutputExport->setMenu(exportMenu);
-		toolButtonOutputExport->setPopupMode(QToolButton::InstantPopup);
-		actionOutputClear = new QAction(QIcon::fromTheme("edit-clear"), gettext("Clear output"), widget);
-		actionOutputClear->setToolTip(gettext("Clear output"));
-		actionOutputReplace = new QAction(QIcon::fromTheme("edit-find-replace"), gettext("Find and Replace"), widget);
-		actionOutputReplace->setToolTip(gettext("Find and replace"));
-		actionOutputReplace->setCheckable(true);
-		actionOutputReplaceKey = new QAction(widget);
-
-		actionOutputSettings = new QAction(QIcon::fromTheme("preferences-system"), gettext("Output Window Preferences"));
-		outputDialog = new QDialog(MAIN);
-		outputDialogUi.setupUi(outputDialog);
-
-		toolBarOutput = new QToolBar(widget);
-		toolBarOutput->setToolButtonStyle(Qt::ToolButtonIconOnly);
-		toolBarOutput->setIconSize(QSize(1, 1) * toolBarOutput->style()->pixelMetric(QStyle::PM_SmallIconSize));
-		toolBarOutput->addWidget(toolButtonInsertMode);
-		toolBarOutput->addSeparator();
-		toolBarOutput->addWidget(toolButtonOpen);
-		toolBarOutput->addAction(actionOutputSaveHOCR);
-		toolBarOutput->addWidget(toolButtonOutputExport);
-		toolBarOutput->addAction(actionOutputClear);
-		toolBarOutput->addSeparator();
-		toolBarOutput->addAction(actionOutputReplace);
-
-		QWidget* spacer = new QWidget(toolBarOutput);
-		spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-		toolBarOutput->addWidget(spacer);
-
-		toolBarOutput->addAction(actionOutputSettings);
-		
-		widget->addAction(actionOutputReplaceKey); // to some harmless parent
-
-		widget->layout()->addWidget(toolBarOutput);
-
-		searchFrame = new SearchReplaceFrame(widget);
-		searchFrame->setVisible(false);
-		widget->layout()->addWidget(searchFrame);
-
-		splitter = new QSplitter(Qt::Vertical, widget);
-		widget->layout()->addWidget(splitter);
-
-		QWidget* treeContainer = new QWidget(widget);
-		treeContainer->setLayout(new QVBoxLayout());
-		treeContainer->layout()->setSpacing(0);
-		treeContainer->layout()->setContentsMargins(0, 0, 0, 0);
-		splitter->addWidget(treeContainer);
-
-		treeViewHOCR = new TreeViewHOCR(widget);
-		treeViewHOCR->setHeaderHidden(true);
-		treeViewHOCR->setSelectionMode(QTreeWidget::ExtendedSelection);
-		treeContainer->layout()->addWidget(treeViewHOCR);
-
-		actionNavigateNext = new QAction(QIcon::fromTheme("go-down"), gettext("Next (F3)"), widget);
-		actionNavigatePrev = new QAction(QIcon::fromTheme("go-up"), gettext("Previous (Shift+F3)"), widget);
-		comboBoxNavigate = new QComboBox();
-		comboBoxNavigate->setSizeAdjustPolicy(QComboBox::AdjustToContents);
-		comboBoxNavigate->setMinimumContentsLength(10);
-		actionExpandAll = new QAction(QIcon(":/icons/expand"), gettext("Expand all"), widget);
-		actionCollapseAll = new QAction(QIcon(":/icons/collapse"), gettext("Collapse all"), widget);
-
-		toolBarNavigate = new QToolBar(widget);
-		toolBarNavigate->setToolButtonStyle(Qt::ToolButtonIconOnly);
-		toolBarNavigate->setIconSize(QSize(1, 1) * toolBarOutput->style()->pixelMetric(QStyle::PM_SmallIconSize));
-		toolBarNavigate->addWidget(comboBoxNavigate);
-		toolBarNavigate->addSeparator();
-		toolBarNavigate->addAction(actionNavigateNext);
-		toolBarNavigate->addAction(actionNavigatePrev);
-		toolBarNavigate->addSeparator();
-		toolBarNavigate->addAction(actionExpandAll);
-		toolBarNavigate->addAction(actionCollapseAll);
-		treeContainer->layout()->addWidget(toolBarNavigate);
-
-		tabWidgetProps = new QTabWidget(widget);
-
-		tableWidgetProperties = new QTableWidget(widget);
-		tableWidgetProperties->setColumnCount(2);
-		tableWidgetProperties->horizontalHeader()->setVisible(false);
-		tableWidgetProperties->verticalHeader()->setVisible(false);
-		tableWidgetProperties->horizontalHeader()->setStretchLastSection(true);
-		tabWidgetProps->addTab(tableWidgetProperties, gettext("Properties"));
-
-		plainTextEditOutput = new OutputTextEdit(widget);
-		plainTextEditOutput->setReadOnly(true);
-		tabWidgetProps->addTab(plainTextEditOutput, gettext("Source"));
-
-		splitter->addWidget(tabWidgetProps);
-	}
+	void setupUi(QWidget* widget);
 };
 
 #endif // UI_OUTPUTEDITORHOCR_HH
